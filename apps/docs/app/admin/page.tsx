aa07466949f1726ac5d17fc7b1076a43e1fb06c9
Aniruddho Roy <aniruddho71@gmail.com>
2025-11-28 22:58:52 +0600

    init

---
+++ apps/docs/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@repo/ui/button"
+export default function Home(){
+    return <div>
+        <Button appName="Radha Krishna">
+            click me
+        </Button>
+    </div>
+}
